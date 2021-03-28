f934dc5d81aa4dc78ae7417f7e578b1b78a3c6be
Luis Jesus Diaz Manzo <luis@drconopoima.com>
2021-03-28 21:37:04 +0200

    Use Chi w/logger middleware, port from ENV

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/drconopoima/key-value-server-go
 
 go 1.16
+
+require github.com/go-chi/chi/v5 v5.0.2 // indirect
--- server.go
+++ server.go
@@ -1,14 +1,25 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello world\n")
+	// Get port from environment variables (default: 8080)
+	port := "8080"
+	if fromEnv := os.Getenv("PORT"); fromEnv != "" {
+		port = fromEnv
+	}
+
+	router := chi.NewRouter()
+	router.Use(middleware.Logger)
+	router.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]uint8("Hello world."))
 	})
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(":"+port, router))
 }
