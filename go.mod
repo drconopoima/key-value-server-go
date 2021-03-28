78cf9ec59c3e39120916cae52fd9263fa77a91ca
Luis Jesus Diaz Manzo <luis@drconopoima.com>
2021-03-28 21:14:24 +0200

    Hello world server port 8080

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/drconopoima/key-value-server-go
+
+go 1.16
---
+++ server.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello world\n")
+	})
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
