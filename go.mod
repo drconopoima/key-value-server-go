9f3a47be83515f19986f76862d28d8742aa9c4b2
Luis Jesus Diaz Manzo <luis@drconopoima.com>
2021-04-03 17:53:22 +0200

    Add minimal key-value GET/DELETE/SET functionality

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/drconopoima/key-value-server-go
 
 go 1.16
 
-require github.com/go-chi/chi/v5 v5.0.2 // indirect
+require github.com/go-chi/chi/v5 v5.0.2
--- server.go
+++ server.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
+	"io"
 	"log"
 	"net/http"
 	"os"
@@ -9,6 +11,8 @@ import (
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
+
+var data = map[string]string{}
 
 func main() {
 	// Get port from environment variables (default: 8080)
@@ -19,8 +23,41 @@ func main() {
 	log.Printf("Starting up server on http://localhost:%s", port)
 	router := chi.NewRouter()
 	router.Use(middleware.Logger)
-	router.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		JSON_RESPONSE(w, map[string]map[string]string{"data": {"message": "Hello World"}})
+	router.Get("/key/{key}", func(writer_get http.ResponseWriter, request_get *http.Request) {
+		key := chi.URLParam(request_get, "key")
+		data, err := Get(request_get.Context(), key)
+		if err != nil {
+			writer_get.WriteHeader(http.StatusInternalServerError)
+			JSON_RESPONSE(writer_get, map[string]string{"error": err.Error()})
+			return
+		}
+		JSON_RESPONSE(writer_get, data)
+	})
+	router.Delete("/key/{key}", func(writer_delete http.ResponseWriter, request_delete *http.Request) {
+		key := chi.URLParam(request_delete, "key")
+		err := Delete(request_delete.Context(), key)
+		if err != nil {
+			writer_delete.WriteHeader(http.StatusInternalServerError)
+			JSON_RESPONSE(writer_delete, map[string]string{"error": err.Error()})
+			return
+		}
+		JSON_RESPONSE(writer_delete, map[string]string{"status": "success"})
+	})
+	router.Post("/key/{key}", func(writer_set http.ResponseWriter, request_set *http.Request) {
+		key := chi.URLParam(request_set, "key")
+		body, err := io.ReadAll(request_set.Body)
+		if err != nil {
+			writer_set.WriteHeader(http.StatusInternalServerError)
+			JSON_RESPONSE(writer_set, map[string]string{"error": err.Error()})
+			return
+		}
+		err = Set(request_set.Context(), key, string(body))
+		if err != nil {
+			writer_set.WriteHeader(http.StatusInternalServerError)
+			JSON_RESPONSE(writer_set, map[string]string{"error": err.Error()})
+			return
+		}
+		JSON_RESPONSE(writer_set, map[string]string{"status": "success"})
 	})
 	log.Fatal(http.ListenAndServe(":"+port, router))
 }
@@ -34,4 +71,18 @@ func JSON_RESPONSE(w http.ResponseWriter, data interface{}) {
 		JSON_RESPONSE(w, map[string]string{"error": err.Error()})
 	}
 	w.Write(json_bytes)
+}
+
+func Get(context context.Context, key string) (string, error) {
+	return data[key], nil
+}
+
+func Set(context context.Context, key string, value string) error {
+	data[key] = value
+	return nil
+}
+
+func Delete(context context.Context, key string) error {
+	delete(data, key)
+	return nil
 }
