bd428568450ff9572b46386d67458cef3e51380f
Luis Jesus Diaz Manzo <luis@drconopoima.com>
2021-04-05 21:55:07 +0200

    Atomic saveData write temporary file then rename

--- server.go
+++ server.go
@@ -5,6 +5,7 @@ import (
 	"encoding/base64"
 	"encoding/json"
 	"io"
+	"io/fs"
 	"log"
 	"net/http"
 	"os"
@@ -190,7 +191,19 @@ func saveData(base64Data *dataVessel, dataFile string) error {
 	if err != nil {
 		return err
 	}
-	return os.WriteFile(dataFile, byteData, 0644)
+	// Preserve existing permissions, default 0644
+	var fileMode fs.FileMode = 0644
+	fileInfo, err := os.Stat(dataFile)
+	if err == nil {
+		fileMode = fileInfo.Mode()
+	}
+	// Write into temporary file
+	err = os.WriteFile(dataFile+".tmp", byteData, fileMode)
+	if err != nil {
+		return err
+	}
+	// Rename temporary file for atomicity
+	return os.Rename(dataFile+".tmp", dataFile)
 }
 
 func encode(text string) string {
