d3620c5a3dc7672b955a3015212589c40a49c6b9
Luis Jesus Diaz Manzo <luis@drconopoima.com>
2021-04-11 19:59:04 +0200

    Refactor: table of test cases

--- server_test.go
+++ server_test.go
@@ -7,17 +7,21 @@ import (
 )
 
 func TestJSON(test *testing.T) {
-	input := map[string]map[string]string{"data": {"message": "Hello world"}}
-	output := `{"data":{"message":"Hello world"}}`
+	testCases := []struct {
+		input  map[string]string
+		output string
+	}{
+		{map[string]string{"message": "Hello world"}, `{"message":"Hello world"}`},
+	}
 	responseRecorder := httptest.NewRecorder()
-	JSON(responseRecorder, input)
+	JSON(responseRecorder, testCases[0].input)
 	response := responseRecorder.Result()
 	defer response.Body.Close()
 	jsonGot, err := io.ReadAll(response.Body)
 	if err != nil {
 		test.Fatalf("Error reading response body: %s", err)
 	}
-	if string(jsonGot) != output {
-		test.Errorf("Output: %s, expected: %s", jsonGot, output)
+	if string(jsonGot) != testCases[0].output {
+		test.Errorf("Output: %s, expected: %s", jsonGot, testCases[0].output)
 	}
 }
