044dfd1b4eb19ac455d3a0d5da6065a431f4d4fb
Luis Jesus Diaz Manzo <luis@drconopoima.com>
2021-04-11 20:36:49 +0200

    Add test cases for header and HTTP status

--- server_test.go
+++ server_test.go
@@ -2,26 +2,43 @@ package main
 
 import (
 	"io"
+	"net/http"
 	"net/http/httptest"
 	"testing"
 )
 
 func TestJSON(test *testing.T) {
+	header := http.Header{}
+	headerKey := "Content-Type"
+	headerValue := "application/json; charset=utf-8"
+	header.Add(headerKey, headerValue)
 	testCases := []struct {
-		input  map[string]string
+		input  interface{}
+		header http.Header
 		output string
+		status string
 	}{
-		{map[string]string{"message": "Hello world"}, `{"message":"Hello world"}`},
+		{map[string]string{"message": "Hello world"}, header, `{"message":"Hello world"}`, "200 OK"},
+		{map[string]map[string]string{"data": {"tables": "testing"}}, header, `{"data":{"tables":"testing"}}`, "200 OK"},
+		{make(chan bool), header, `{"error":"json: unsupported type: chan bool"}`, "500 Internal Server Error"},
 	}
-	responseRecorder := httptest.NewRecorder()
-	JSON(responseRecorder, testCases[0].input)
-	response := responseRecorder.Result()
-	defer response.Body.Close()
-	jsonGot, err := io.ReadAll(response.Body)
-	if err != nil {
-		test.Fatalf("Error reading response body: %s", err)
-	}
-	if string(jsonGot) != testCases[0].output {
-		test.Errorf("Output: %s, expected: %s", jsonGot, testCases[0].output)
+	for _, testCase := range testCases {
+		responseRecorder := httptest.NewRecorder()
+		JSON(responseRecorder, testCase.input)
+		response := responseRecorder.Result()
+		defer response.Body.Close()
+		jsonGot, err := io.ReadAll(response.Body)
+		if err != nil {
+			test.Fatalf("Error reading response body: %s", err)
+		}
+		if string(jsonGot) != testCase.output {
+			test.Errorf("Output: %s, expected: %s", jsonGot, testCase.output)
+		}
+		if contentType := response.Header.Get(headerKey); contentType != headerValue {
+			test.Errorf("Output: %s, expected: %s", contentType, headerValue)
+		}
+		if status, testStatus := response.Status, testCase.status; status != testStatus {
+			test.Errorf("Output: %s, expected: %s", status, testStatus)
+		}
 	}
 }
