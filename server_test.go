945033e9fae146ecbb47310b725135a94417a1e2
Luis Jesus Diaz Manzo <luis@drconopoima.com>
2021-04-11 22:04:25 +0200

    Initial testGet. Create testdata and load known

--- server_test.go
+++ server_test.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"context"
+	"encoding/base64"
+	"encoding/json"
 	"io"
 	"net/http"
 	"net/http/httptest"
+	"os"
 	"testing"
 )
+
+var directoryName string = "testdata"
 
 func TestJSON(test *testing.T) {
 	header := http.Header{}
@@ -29,16 +35,51 @@ func TestJSON(test *testing.T) {
 		defer response.Body.Close()
 		jsonGot, err := io.ReadAll(response.Body)
 		if err != nil {
-			test.Fatalf("Error reading response body: %s", err)
+			test.Fatalf("Error reading response body: %v", err)
 		}
 		if string(jsonGot) != testCase.output {
-			test.Errorf("Output: %s, expected: %s", jsonGot, testCase.output)
+			test.Errorf("Output: %v, expected: %v", jsonGot, testCase.output)
 		}
 		if contentType := response.Header.Get(headerKey); contentType != headerValue {
-			test.Errorf("Output: %s, expected: %s", contentType, headerValue)
+			test.Errorf("Output: %v, expected: %v", contentType, headerValue)
 		}
 		if status, testStatus := response.Status, testCase.status; status != testStatus {
-			test.Errorf("Output: %s, expected: %s", status, testStatus)
+			test.Errorf("Output: %v, expected: %v", status, testStatus)
 		}
+	}
+}
+
+func TestGet(test *testing.T) {
+	makeStorage(test)
+	defer cleanupStorage(test)
+	key := "key"
+	value := "value"
+	encodedKey := base64.URLEncoding.EncodeToString([]byte(key))
+	encodedValue := base64.URLEncoding.EncodeToString([]byte(value))
+	fileContents, _ := json.Marshal(map[string]string{encodedKey: encodedValue})
+	dataFileName := directoryName + "/data.json"
+
+	os.WriteFile(dataFileName, fileContents, 0644)
+	loadData(dataFileName, &base64Data, &data)
+	got, err := Get(context.Background(), key)
+	if err != nil {
+		test.Errorf("Unexpected error: %v", err)
+	}
+	if got != value {
+		test.Errorf("Output: %v, expected: %v", got, value)
+	}
+}
+
+func makeStorage(test *testing.T) {
+	err := os.Mkdir(directoryName, 0755)
+	if err != nil {
+		test.Logf("Couldn't create directory '%v'. %v", directoryName, err)
+	}
+}
+
+func cleanupStorage(test *testing.T) {
+	err := os.RemoveAll(directoryName)
+	if err != nil {
+		test.Fatalf("Failed to delete directory '%v'. %v", directoryName, err)
 	}
 }
